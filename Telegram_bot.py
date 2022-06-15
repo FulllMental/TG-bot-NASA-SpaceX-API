daff424ba45d3fafc6515a751306b871ca69aab4
FulllMental <104234625+FulllMental@users.noreply.github.com>
2022-06-15 03:14:40 +0300

    publish photo to Telegram channel

--- Telegram_bot.py
+++ Telegram_bot.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 def bot_message():
     bot = telegram.Bot(token=os.getenv("TELEGRAM_BOT_API"))
-    bot.send_message(chat_id='@nasa_photo_lesson', text='Hello!')
+    bot.send_photo(chat_id='@nasa_photo_lesson', photo=open('images/hubble.jpeg', 'rb'))
 
 
 if __name__ == '__main__':
